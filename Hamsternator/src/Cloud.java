217c1a1aa3a30856015ed4784f721a4379a6007b
Ethan Smith <nathan.noyou@gmail.com>
2025-05-29 08:57:33 -0700

    asdf

---
+++ Hamsternator/src/Cloud.java
@@ -0,0 +1,33 @@
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Image;
+import java.awt.Toolkit;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.geom.AffineTransform;
+import java.net.URL;
+
+public class Cloud extends Sprite{
+	
+	private int x;
+	private int y;
+	
+	public Cloud() {
+		super("pictures/Cloud1.png");
+		x = 0;
+		y = 0;
+	}
+	
+	public void change1() {
+		this.changePicture("pictures/Cloud1.png");
+	}
+	public void change2() {
+		this.changePicture("pictures/Cloud2.png");
+
+	}
+	public void change3() {
+		this.changePicture("pictures/cloud3.png");
+
+	}
+	
+}
