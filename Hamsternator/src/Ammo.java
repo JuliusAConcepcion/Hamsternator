fb4638df24417e1f82cf47e2a7e522b328893b5d
Ethan Smith <nathan.noyou@gmail.com>
2025-06-10 09:14:23 -0700

    ammo sprite added

---
+++ Hamsternator/src/Ammo.java
@@ -0,0 +1,12 @@
+
+public class Ammo extends Sprite{
+	
+	public Ammo() {
+		
+		super("pictures/Shell.png");
+		this.setScaleX(7);
+		this.setScaleY(7);
+		this.setX(450);
+		this.setY(960);
+	}
+}
